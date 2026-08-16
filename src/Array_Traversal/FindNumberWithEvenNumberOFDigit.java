81ab437d24585c2f8e3fc9a05929c9a0c27d61c0
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-08-16 20:19:02 +0530

    added

--- src/Array_Traversal/FindNumberWithEvenNumberOFDigit.java
+++ src/Array_Traversal/FindNumberWithEvenNumberOFDigit.java
@@ -1,4 +1,11 @@
 package Array_Traversal;
 
 public class FindNumberWithEvenNumberOFDigit {
+
+    public static int findNumbers(int[] nums){
+
+    }
+    public static void main(String[] args) {
+
+    }
 }
