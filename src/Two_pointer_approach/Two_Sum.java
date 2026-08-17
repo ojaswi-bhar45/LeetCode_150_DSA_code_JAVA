2f326950a825e082304ec92cb8bc7bd91949dea3
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-08-17 14:50:38 +0530

    done

--- src/Two_pointer_approach/Two_Sum.java
+++ src/Two_pointer_approach/Two_Sum.java
@@ -3,4 +3,35 @@
 package Two_pointer_approach;
 
 public class Two_Sum {
+    public static int[] twoSum(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+
+        while (left < right) {
+            int sum = nums[left] + nums[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+        int[] nums2 = { 2, 3, 4};
+        int target2 = 6;
+
+        int[] result = twoSum(nums, target);
+        int[] result2 = twoSum(nums2, target2);
+
+        System.out.println("[" + result[0] + ", " + result[1] + "]");
+        System.out.println("[" + result2[0] + ", " + result2[1] + "]");
+    }
 }
