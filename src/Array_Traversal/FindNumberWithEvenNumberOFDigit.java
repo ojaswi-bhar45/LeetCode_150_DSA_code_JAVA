bd2694bca8dfc99f59d1918d6c17293638c869d2
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-08-17 14:22:49 +0530

    done

--- src/Array_Traversal/FindNumberWithEvenNumberOFDigit.java
+++ src/Array_Traversal/FindNumberWithEvenNumberOFDigit.java
@@ -3,9 +3,29 @@ package Array_Traversal;
 public class FindNumberWithEvenNumberOFDigit {
 
     public static int findNumbers(int[] nums){
+        int count = 0;
+        for(int num: nums){
+            int digits = 0;
+            while(num > 0){
+                num = num / 10;
+                digits ++;
+            }
+
+            if(digits % 2 == 0){
+                count ++;
+            }
+        }
+        return count;
 
     }
     public static void main(String[] args) {
+        int[] nums = {12, 345, 2, 6, 7896};
+        int[] nums2 = {555, 901, 482, 1771};
+
+        int result = findNumbers(nums);
+        int result2 = findNumbers(nums2);
+        System.out.println(result);
+        System.out.println(result2);
 
     }
 }
