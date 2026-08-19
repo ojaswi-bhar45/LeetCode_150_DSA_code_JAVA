91f5803db531a4367b9f856f64d234a4398e970e
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-08-19 21:41:14 +0530

    done reverse string

--- src/Two_pointer_approach/Reverse_String.java
+++ src/Two_pointer_approach/Reverse_String.java
@@ -3,4 +3,24 @@
 package Two_pointer_approach;
 
 public class Reverse_String {
+    public static void reverseString(char[] s){
+        int left = 0;
+        int right = s.length - 1;
+
+        while(left < right){
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+
+            left ++;
+            right --;
+        }
+    }
+    public static void main(String[] args) {
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        System.out.println("Before: " + java.util.Arrays.toString(s));
+        reverseString(s);
+        System.out.println("After:  " + java.util.Arrays.toString(s));
+    }
 }
