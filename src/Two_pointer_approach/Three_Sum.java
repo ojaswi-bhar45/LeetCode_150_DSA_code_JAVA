f8a03f255be8b3b0258de802729cd4a51a92b084
Ojaswi bhardwaj <ojaswibhardwaj86@gmail.com>
2026-08-19 21:49:44 +0530

    done

--- src/Two_pointer_approach/Three_Sum.java
+++ src/Two_pointer_approach/Three_Sum.java
@@ -2,5 +2,55 @@
 
 package Two_pointer_approach;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Three_Sum {
+    public static  List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        Arrays.sort(nums);
+        for(int i = 0 ; i < nums.length - 2 ; i ++){
+            //Skip the duplicates first element
+
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while(left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    left ++;
+                    right --;
+                    // Skip duplicate left values
+                    while (left < right &&
+                            nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    // Skip duplicate right values
+                    while (left < right &&
+                            nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                }else if(sum < 0){
+                    left ++;
+                }else{
+                    right --;}
+            }}
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> res = threeSum(nums);
+        System.out.println(res);
+
+    }
 }
